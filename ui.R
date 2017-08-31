821a0691c1164c82f7aeabc50e948d8cf4aaa9ff
Trentham3269 <scottdalwood@gmail.com>
2017-08-31 14:03:51 +1000

    Include state as first UI select element

--- ui.R
+++ ui.R
@@ -8,20 +8,22 @@ shinyUI(
     # # Include Google Analytics
     # tags$head(includeScript("www/google-analytics.js")),
     
-    h2(titlePanel("")),
-    h3(titlePanel("")),
+    h2(titlePanel("National Rifle Association of Australia")),
+    h3(titlePanel("Range Locations")),
     br(),
     
     sidebarLayout(
 
       sidebarPanel(width = 4,
                    
-
-        
-        p(paste("Data is current as at", format(Sys.Date(), "%d.%m.%Y")))
+        selectInput(inputId   = "state"
+                    , label   = "Filter by State"
+                    , choices = c("All", levels(factor(df$State)))),
+                   
+       p(paste("Data is current as at", format(Sys.Date(), "%d.%m.%Y")))
+      
       ),
       
-      # Data Table
       mainPanel( 
            
       )
