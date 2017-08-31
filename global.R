02733c095b5ff97072c339acbcc1ae7eba088895
Trentham3269 <scottdalwood@gmail.com>
2017-08-31 13:54:04 +1000

    Include range data and read into application

--- global.R
+++ global.R
@@ -1,5 +1,10 @@
 suppressPackageStartupMessages({
   library(shiny)        # web framework
   library(shinythemes)  # free bootstrap themes from bootswatch.com
+  library(readr)        # fast I/O
+  library(dplyr)        # data wrangling
+  library(leaflet)      # leaflet js for R
 })
 
+# Read data 
+df <- read_csv("data/df.csv")
